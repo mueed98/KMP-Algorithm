fd39ba32ac182b2747a72f70b809c584838334fb
Mueed-cysec <62989197+Mueed-cysec@users.noreply.github.com>
2020-04-01 16:47:14 +0500

    Update PartAQ2.cpp

--- PartAQ2.cpp
+++ PartAQ2.cpp
@@ -11,16 +11,18 @@ void foo(string pattern, string String)
 	while (i <= N - M) 
 	{ 
 		int j;
+		//simple comparision
 		for (j = 0; j < M; j++) 
 			if (String[i + j] != pattern[j]) 
-				break; 
+				break;
+		// simple case of pattern found
 		if (j == M) 
-		{ 
-			cout << "Pattern found at index " << i << endl; 
-			i = i + M; 
-		} 
-		else if (j == 0) 
+		{ cout << "Pattern found at index " << i << endl; 
+			i = i + M; }
+		// case where moving to next string alphabet to match full pattern because we have already scanned the previos string alphabets
+		else if (j == 0)  
 			i = i + 1; 
+		//case where after mismatch we are avoiding the already matched string alpabets and are jumping to i + j 
 		else
 			i = i + j;
 	} 
