31f0790bda3f09cc0a2f9df21a8d20a3c79add97
Mueed-cysec <62989197+Mueed-cysec@users.noreply.github.com>
2020-04-01 22:04:00 +0500

    Create PartBQ1KMP.cpp

---
+++ PartBQ1KMP.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+void foo( int pie[], string str ,int length)
+{
+    int j=0; 
+    pie[0]=0;  //because the value always starts from 0
+    for( int i=1; i<length; i++ ) 
+    {
+        if ( str[i] == str[j] )
+        {   j++;
+        	pie[i]=j; //since its a match so saving pie value
+        }
+        else
+        j=0; //no match so j=0;
+        if( str[i] == str[j] )
+        j++; //since matching again so incremented value has to be saved as pie 
+        pie[i]=j; 
+    }
+	
+}
+
+
+int main()
+{
+    int pie[9];
+    string str="mueedmueedmueed";
+    foo(pie, str, 9);
+    for( int i=0; i<9; i++)
+    	cout<<pie[i]<<"\t";
+    return 0;
+
+}
